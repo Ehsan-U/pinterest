7465b0fcde21503a549fe2e089ec9eb076373390
EhsanUllah <88642086+Ehsan-U@users.noreply.github.com>
2023-03-12 12:17:21 +0500

    pagination fixed

--- Pinterest_Scraper.py
+++ Pinterest_Scraper.py
@@ -1,40 +1,42 @@
+import random
+
 import scrapy
 import logging
 from scrapy.crawler import CrawlerProcess
 import json
 from CONSTANT import search_parameters, profile_parameters
 from copy import deepcopy
+from urllib.parse import urlencode
 
 
-class Pinterest(scrapy.Spider):
+class Scraper(scrapy.Spider):
     name = 'pinterest_spider'
     resource_endpoint = 'https://www.pinterest.com:443/resource/UserResource/get/?'
-    search_endpoint = 'https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=/search/pins/?'
+    search_endpoint = 'https://www.pinterest.com/resource/BaseSearchResource/get/?'
     channels = set()
     result_counter = 0
-
+    page_no=0
 
     def start_requests(self):
         self.maxResults = self.maxResults
-        keywords = self.keywords
-        for keyword in keywords:
+        for keyword in self.keywords:
             params = self.build_params(keyword.get("key"))
-            yield scrapy.Request(self.search_endpoint + json.dumps(params), callback=self.parse, cb_kwargs={"keyword":keyword.get("key"), "uid": keyword.get("idOutRequest")})
-
+            url = self.search_endpoint + f'source_url={params.get("source_url")}' + '&data=' + json.dumps(params['data'])
+            yield scrapy.Request(url, callback=self.parse, cb_kwargs={"keyword":keyword.get("key"), "uid": keyword.get("idOutRequest")})
 
     async def parse(self, response, keyword, uid):
+        print(f" \n{self.page_no}\n")
         data = json.loads(response.body)
-        results = data.get("resource_response").get("data").get("results")
+        results = data.get("resource_response", {}).get("data", {}).get("results")
         if results:
             for result in results:
-                idChannel = result.get("pinner").get("id")
+                idChannel = result.get("pinner", {}).get("id")
                 if idChannel not in self.channels:
                     self.channels.add(idChannel)
-                    channelName = result.get("pinner").get("full_name")
-                    username = result.get("pinner").get("username")
-                    channelURL = f'https://www.pinterest.com/{username}/'
-                    metric_Subscribers = result.get("pinner").get("follower_count")
-                    url = self.from_profile_parameters(self.resource_endpoint, username)
+                    channelName = result.get("pinner", {}).get("username")
+                    channelURL = f'https://www.pinterest.com/{channelName}/'
+                    metric_Subscribers = result.get("pinner", {}).get("follower_count")
+                    url = self.from_profile_parameters(self.resource_endpoint, channelName)
                     response = await self.get_url(url)
                     data_helper = response.get("resource_response", {}).get("data")
                     metric_MonthlyViews = data_helper.get("profile_views")
@@ -51,16 +53,17 @@ class Pinterest(scrapy.Spider):
                     )
                     if self.result_counter < self.maxResults:
                         self.result_counter +=1
-                        print(item)
-                        yield item
+                        print(f"[{self.result_counter}] >> {item}")
+                        # yield item
                     else:
                         break
-        # pagination
-        if self.result_counter < self.maxResults:
-            bookmark = data.get("resource").get("options").get("bookmarks")
+        if (self.result_counter < self.maxResults):
+            self.page_no+=1
+            bookmark = data.get('resource_response').get("bookmark")
             params = self.build_params(keyword)
-            params['data']['options'].update({"bookmarks": bookmark})
-            yield scrapy.Request(self.search_endpoint + json.dumps(params), callback=self.parse, cb_kwargs={"keyword": keyword, "uid": uid})
+            params['data']['options'].update({"bookmarks": [bookmark]})
+            url = self.search_endpoint + f'source_url={params.get("source_url")}' + '&data=' + json.dumps(params['data']) + '&_=' + data.get("request_identifier")
+            yield scrapy.Request(url, callback=self.parse, cb_kwargs={"keyword": keyword, "uid": uid})
 
 
     async def get_url(self, url):
@@ -68,22 +71,21 @@ class Pinterest(scrapy.Spider):
         data = json.loads(response.body)
         return data
 
-
     @staticmethod
     def build_params(keyword):
         params = deepcopy(search_parameters)
-        params['q'] = keyword
+        params['source_url'] = f'/search/pins/?q={keyword}'
         params['data']['options']['query'] = keyword
         return params
 
-
     @staticmethod
-    def from_profile_parameters(resource_endpoint, username):
+    def from_profile_parameters(resource_endpoint, channelName):
         profile_params = deepcopy(profile_parameters)
-        profile_params['source_url'] = f"/{username}/"
-        profile_params['data']['options']['username'] = username
+        profile_params['source_url'] = f"/{channelName}/"
+        profile_params['data']['options']['username'] = channelName
         url = resource_endpoint + "source_url=" + profile_params['source_url'] + '&' + "data=" + json.dumps(profile_params['data'])
         return url
+
 
 
 crawler = CrawlerProcess(settings={
@@ -94,12 +96,12 @@ crawler = CrawlerProcess(settings={
         "X-Requested-With": "XMLHttpRequest",
         "Host": "www.pinterest.com"
     },
-    "LOG_LEVEL": logging.INFO,
+    "LOG_LEVEL": logging.DEBUG,
     "LOG_ENABLED": True,
-    "DOWNLOAD_DELAY": 0.5,
+    "DOWNLOAD_DELAY": 3,
     "CONCURRENT_REQUESTS": 8,
     "HTTPCACHE_ENABLED": True,
     "REQUEST_FINGERPRINTER_IMPLEMENTATION": "2.7",
 })
-crawler.crawl(Pinterest, keywords=[{"key": 'playwright', "idOutRequest": 1}], maxResults=10)
+crawler.crawl(Scraper, keywords=[{"key": 'Rumi', "idOutRequest": 1}], maxResults=50)
 crawler.start()
