857f46ad601b43c7aac4b0746cbce69cb936fa98
EhsanUllah <88642086+Ehsan-U@users.noreply.github.com>
2023-03-12 20:36:30 +0500

    update

--- CONSTANT.py
+++ CONSTANT.py
@@ -1,10 +1,10 @@
 search_parameters = {
-    "q":"",
+    "source_url":"",
     "data":{
         "options":{
             "article":"",
             "appliedProductFilters":"---",
-            "query":"playwright",
+            "query":"",
             "scope":"pins",
             "auto_correction_disabled":"",
             "top_pin_id":"","filters":""
@@ -21,6 +21,34 @@ profile_parameters = {
             "field_set_key":"profile",
         },
     "context":{}
+    }
+}
+
+activity_parameters = {
+    "source_url": "",
+    "data": {
+        "options":{
+            "data":{},
+            "exclude_add_pin_rep":True,
+            "field_set_key":"grid_item",
+            "is_own_profile_pins":False,
+            "user_id":"",
+            "username":""
+        },
+        "context":{}
+    }
+}
+
+pin_parameters = {
+    "source_url": "",
+    "data": {
+        "options":{
+            "id":"",
+            "field_set_key":"auth_web_main_pin",
+            "noCache":True,
+            "fetch_visual_search_objects":True
+        },
+        "context":{}
     }
 }
 
